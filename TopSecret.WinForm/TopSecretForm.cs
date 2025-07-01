72f302a1d668f28237ee62d9546f60adfb320ec0
Danny Glenfield <dglenfield@gmail.com>
2025-07-01 11:28:06 -0400

    Add Console and WinForm projects

---
+++ TopSecret.WinForm/TopSecretForm.cs
@@ -0,0 +1,10 @@
+namespace TopSecret.WinForm
+{
+    public partial class TopSecretForm : Form
+    {
+        public TopSecretForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
